2ceecfac0fafe52e3efe6bc835d6512c3a341f3d
Ajay-droid23 <ajays2003s12@gmailcom>
2022-09-23 12:27:52 +0530

    CrypToLock

---
+++ CrypToLock/Frontend/cryptolockui/Frontend/form/pliblockathon/CrypToLock/Frontend/cryptolockui/src/utils.js
@@ -0,0 +1,61 @@
+const getWeb3 = () => {
+    console.log("Iam here 1")
+    return new Promise((resolve, reject) => {
+      window.addEventListener('load', async () => {
+        // Modern dapp browsers...
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum);
+            console.log("Iam here 2")
+            try {
+                // Request account access if needed
+                await ethereum.enable();
+                // Acccounts now exposed
+                await window.ethereum.request({ method: "eth_requestAccounts" });
+                console.log("Iam here 3")
+  
+                resolve(web3);
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+          console.log("Iam here 3")
+  
+            window.web3 = new Web3(web3.currentProvider);
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+      });
+    });
+  };
+  
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      return window.web3;
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+      return window.web3;
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  };
+  
+  const getFarmChainContract = async (web3) => {
+    const data = await $.getJSON("./contracts/abi.json");
+    console.log('data',data);
+    const eco1 = new web3.eth.Contract(
+      data,
+      "0xd9145CCE52D386f254917e481eB44e9943F39138"
+    );
+    console.log("eco1",eco1)
+    return eco1;
+  };
+  
+  
+  
